e235c4f9fd90609ab40dc72854e5b5be5dbe85ea
Andres Torrubia <antorsae@gmail.com>
2018-08-02 16:25:36 +0200

    scripts to build OOF models

---
+++ oof_no_pseudo.sh
@@ -0,0 +1,35 @@
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d                  --drop  -du -cg -mm -md 2 -mb 36 -r c -des -i  891 -n 21 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d                  --drop  -du -cg -mm -md 2 -mb 36 -r l -des -i 2035 -n 22 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d                  --drop  -du -cg -mm -md 2 -mb 36 -r x -des -i  284 -n 20 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d p   s   i   r    --drop  -du -cg -mm -md 2 -mb 36 -r c -des -i 1171 -n  99 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d p l s t          --drop  -du -cg -mm -md 2 -mb 36 -r l -des -i 1159 -n  98 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d   l   t   g   a  --drop  -du -cg -mm -md 2 -mb 36 -r x -des -i  291 -n 100 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d         i g r a  --dropX -du -cg -mm -md 2 -mb 36 -r c -des -i 1027 -n  49 -bh 190 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d p     t i     a  --dropX -du -cg -mm -md 2 -mb 36 -r l -des -i 2240 -n  48 -bh 210 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d p l s t          --dropX -du -cg -mm -md 2 -mb 36 -r x -des -i  280 -n  50 -bh 200 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d p   s            --drop  -du -cg -mm -md 2 -mb 36 -r c -des -i  847 -n 149 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d           g r    --drop  -du -cg -mm -md 2 -mb 36 -r l -des -i 1522 -n 148 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d   l   t          --drop  -du -cg -mm -md 2 -mb 36 -r x -des -i  275 -n 150 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d          i g     --dropX -du -cg -mm -md 2 -mb 36 -r c -des -i  877 -n 199 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d   l           a  --dropX -du -cg -mm -md 2 -mb 36 -r l -des -i 2063 -n 198 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d p   s            --drop  -du -cg -mm -md 2 -mb 36 -r x -des -i  295 -n 200 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d   l              --dropX -du -cg -mm -md 2 -mb 36 -r c -des -i 927 -n 219 -bh 310 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d       t          --dropX -du -cg -mm -md 2 -mb 36 -r l -des -i 1836 -n 218 -bh 290 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d            g     --dropX -du -cg -mm -md 2 -mb 36 -r x -des -i  284 -n 220 -bh 300 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d     s      g     --dropX -du         -md 6 -mb  2 -r c -des -i 2005 -n  81 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d         i   r    --drop  -du         -md 6 -mb  2 -r l -des -i 1522 -n  78 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d   l   t          --drop  -du         -md 6 -mb  2 -r x -des -i  300 -n  80 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d         i   r    --dropX -du         -md 6 -mb  2 -r c -des -i 1267 -n 119 -bh 49 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d     s     g      --dropX -du         -md 6 -mb  2 -r l -des -i 1403 -n 118 -bh 51 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d p l   t          --dropX -du         -md 6 -mb  2 -r x -des -i  289 -n 120 -bh 50 $2 ; fi
+
+if [ $1 = "c" ]; then python mxauto.py -f 5 -d                  --dropX -du         -md 6 -mb  2 -r c -des -i 1565 -n  39 $2 ; fi
+if [ $1 = "l" ]; then python mxauto.py -f 5 -d                  --drop  -du         -md 6 -mb  2 -r l -des -i 1225 -n  38 $2 ; fi
+if [ $1 = "x" ]; then python mxauto.py -f 5 -d                  --drop  -du         -md 6 -mb  2 -r x -des -i  281 -n  40 $2 ; fi
